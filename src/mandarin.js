a10a977a252df326bdb5314f6d756784097840d0
Henrik Rinne <henrik.rinne@activearkjwt.com>
2015-04-29 16:20:59 +0300

    added wait, pauseAll, resumeAll, set

--- src/mandarin.js
+++ src/mandarin.js
@@ -1,30 +1,43 @@
-var Mandarin2 = function (name) {
+var Mandarin = function (name) {
 
 
 
 
-        Mandarin2.objectsContainer = Mandarin2.objectsContainer ? Mandarin2.objectsContainer : [];
+        Mandarin.objectsContainer = Mandarin.objectsContainer ? Mandarin.objectsContainer : [];
 
-        Mandarin2.pauseAll = function () {
-            console.log("pause");
-            var c = Mandarin2.objectsContainer;
+        Mandarin.pauseAll = function () {
+
+            var c = Mandarin.objectsContainer;
             for(var i = 0; i <  c.length; i++) {
-                c[i].style.webkitAnimationPlayState="paused";
-                c[i].style.animationPlayState="paused";
+                c[i].pause();
             }
 
         };
 
 
-        Mandarin2.resumeAll = function () {
-            console.log("resume");
-            var c = Mandarin2.objectsContainer;
+        Mandarin.resumeAll = function () {
+
+            var c = Mandarin.objectsContainer;
             for(var i = 0; i <  c.length; i++) {
-                c[i].style.webkitAnimationPlayState="running";
-                c[i].style.animationPlayState="running";
+                c[i].resume();
             }
 
         };
+
+
+        Mandarin.startOver = function () {
+
+            var c = Mandarin.objectsContainer;
+            for(var i = 0; i <  c.length; i++) {
+                c[i].startOver();
+
+            }
+
+            Mandarin.resumeAll();
+
+        };
+
+
 
         var animObject = {};
 
@@ -35,11 +48,12 @@ var Mandarin2 = function (name) {
 
         var animElement = document.getElementById(name);
 
+        animObject.animElement = animElement;
 
         // add the DOM object to array, for future use
-        Mandarin2.objectsContainer.push(animElement);
+        Mandarin.objectsContainer.push(animObject);
 
-        console.log(Mandarin2.objectsContainer);
+
 
 
         animElement.style.position = "absolute";
@@ -64,7 +78,7 @@ var Mandarin2 = function (name) {
         var animationDelay = [0];
 
 
-        //console.log(anim);
+
 
         var y = 0;
         var x = 0;
@@ -83,6 +97,19 @@ var Mandarin2 = function (name) {
         return animObject;
 
     };
+
+
+    animObject.pause = function () {
+        animElement.style.webkitAnimationPlayState="paused";
+        animElement.style.animationPlayState="paused";
+        return animObject;
+    }
+
+    animObject.resume = function () {
+        animElement.style.webkitAnimationPlayState="running";
+        animElement.style.animationPlayState="running";
+        return animObject;
+    }
 
 
     animObject.wait = function(time) {
@@ -201,7 +228,8 @@ var Mandarin2 = function (name) {
             var that = animObject;
 
             setTimeout(function () {
-
+                animElement.style.webkitAnimationPlayState="running";
+                animElement.style.animationPlayState="running";
                 animElement.className += " " + that.animationName + "_class";
             }, 10)
 
@@ -233,12 +261,7 @@ var Mandarin2 = function (name) {
             style.sheet.insertRule("." + animObject.animationName + "_class {" + browserPrefix + "animation:" + str1 + "; " + browserPrefix + "animation-delay:" + str2 + "; " + browserPrefix + "animation-fill-mode:forwards}", 0);
             animElement.className += " " + animObject.animationName + "_class";
 
-            //console.log(str1);
-            //animElement.style[animation_text] =  str1;
 
-            //console.log(str2);
-
-            //animElement.style[animationDelay_text] = str2;
 
             animObject.str1 = str1;
             animObject.str2 = str2;
@@ -249,181 +272,3 @@ var Mandarin2 = function (name) {
         return animObject;
 
 }
-
-
-/*
-var Mandarin = function (name) {
-    var animNum = 0;
-
-    var n_name = name;
-
-    var animElement = document.getElementById(name);
-    animElement.style.position ="absolute";
-
-    var animation_text = "animation";
-    var transform_text = "transform";
-    var animationDelay_text = "animationDelay";
-    var browserPrefix  ="";
-
-
-    if(animElement.style.webkitAnimation !== undefined) { // webkit
-        animation_text = "webkitAnimation";
-        animationDelay_text = "webkitAnimationDelay";
-        browserPrefix = "-webkit-";
-        transform_text = "webkitTransform";
-
-    }
-
-    var animSquence = [];
-    var easingSequence = [];
-    var animationTime = [];
-    var animationDelay = [0];
-
-
-    //console.log(anim);
-
-    var y= 0;
-    var x = 0;
-    var rotation = 0;
-    var scale = 1;
-    var alpha = 1;
-    var ease = "ease-out"
-    var delayNum = 0;
-
-
-    this.addAnim = function (time, obj) {
-
-
-
-        var animationName = "animationMaker"+ n_name + "_"+ animNum ;
-        this.animationName = animationName;
-        animationDelay.push(time);
-        animationTime.push(time);
-
-        //scale, rotate, alpha, x, y;
-
-        var sec = "{0% {} 100% {}}";
-
-        if(obj){
-
-            var percent0 ="";
-            var percent100 = "";
-
-            //delay
-            delayNum = (obj.delay!== undefined) ? obj.delay : 0;
-            //animationDelay[animNum]+= 1;
-            //easing
-            ease = (obj.ease!== undefined) ? obj.ease : "linear";
-
-
-            // y
-            percent0 += "top:" + y + "px; " ;
-            y = (obj.y!== undefined) ? obj.y : y;
-            percent100 += "top:" + y + "px; " ;
-
-
-            //x
-            percent0 += "left:" + x + "px; " ;
-            x = (obj.x!== undefined) ? obj.x : x;
-            percent100 += "left:" + x + "px; " ;
-
-
-            // transform
-
-
-            var transform = "translate("+ (-animElement.clientWidth*.5) + "px,"+ (-animElement.clientHeight*.5) +"px)";
-            //var transform = "translate(500px,0 ) ";
-            //var transform = "";
-            //rotation 0%
-            transform += " rotate(" +rotation + "deg) " ;
-
-            //scale
-            transform += "scale(" +scale + ","+scale+") " ;
-            percent0 += browserPrefix + "transform:" + transform+";";
-
-
-            scale = (obj.scale!== undefined) ? obj.scale : scale;
-            rotation = (obj.rotation!== undefined) ? obj.rotation : rotation;
-
-            transform = "translate("+ (-animElement.clientWidth*.5) + "px,"+ (-animElement.clientHeight*.5) +"px)";
-            transform += "rotate(" +rotation + "deg) " ;
-            //scale
-            transform += "scale(" +scale + ","+scale+") " ;
-            percent100 += browserPrefix + "transform:" + transform+";";
-
-
-
-            //alpha
-            percent0 += "opacity:"+alpha+"; " ;
-            alpha = (obj.alpha !== undefined) ? obj.alpha : alpha;
-            percent100  += "opacity:"+alpha+"; " ;
-
-            console.log(percent0, percent100);
-            sec = "{0%{"+percent0+"} 100% {"+percent100+"} }";
-
-
-            //x = obj.x;
-
-        }
-
-        var keyframes = "@"+browserPrefix+"keyframes "+animationName+" " + sec;
-        easingSequence.push(ease);
-        animSquence.push(animationName);
-
-        //alert(animationName);
-
-
-        document.styleSheets[0].insertRule( keyframes, 0 );
-
-
-        animNum++;
-
-        //console.log(animElement);
-    }
-
-
-    this.startOver = function() {
-
-        animElement.className  = "";
-        var that = this;
-
-        setTimeout(function () {
-
-            animElement.className += " " +that.animationName  +"_class";
-        },10)
-
-
-
-    }
-
-    this.startAnim = function () {
-        var str1 = "";
-        var str2 = "";
-        var delay = 0;
-        for(var i =  0; i<animSquence.length; i++) {
-            str1 += animSquence[i]+ " "+animationTime[i]+"s "+easingSequence[i];
-            if(i != animSquence.length-1) str1 += ",";
-            delay += animationDelay[i] ;
-            str2 += delay+ "s";
-            if(i != animSquence.length-1) str2 += ",";
-
-        }
-
-
-        document.styleSheets[0].insertRule( "." + this.animationName  +"_class {"+browserPrefix+"animation:"+str1+"; "+browserPrefix+"animation-delay:"+str2+"; "+browserPrefix+"animation-fill-mode:forwards}" ,0);
-        animElement.className += " " +this.animationName  +"_class";
-
-        //console.log(str1);
-        //animElement.style[animation_text] =  str1;
-
-        //console.log(str2);
-
-        //animElement.style[animationDelay_text] = str2;
-
-        this.str1 = str1;
-        this.str2 = str2;
-
-    }
-
-}
-*/
